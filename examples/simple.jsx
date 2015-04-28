29b183ee9417667c7f0ec1bb907176c7a24a8afa
Daiderd Jordan <daiderd@gmail.com>
2015-04-28 17:49:00 +0200

    fixed example

--- examples/simple.jsx
+++ examples/simple.jsx
@@ -4,18 +4,18 @@ var React = require('react/addons')
   , { List, Item, Value } = require('./')
   ;
 
-var  = React.createClass({
+var Example = React.createClass({
   mixins : [PureRenderMixin]
 ,
   render : function () {
-    var data = [
+    var list = [
       { first_name: 'Daiderd', last_name: 'Jordan' }
     , { first_name: 'John', last_name: 'Doe' }
     , { first_name: 'Jane', last_name: 'Doe' }
     ];
 
     return (
-      <List list={this.props.collection}>
+      <List list={list}>
         <Item>
           <Value get={el => el.first_name} />
           <Value get={el => el.last_name} />
